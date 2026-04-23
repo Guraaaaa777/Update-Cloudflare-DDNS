74ca60fe2a22e28d2ff3a0a56d3581a4a240762d
gura <guraaaaa777@gmail.com>
2026-04-23 17:07:35 +0900

    emplement change record query

--- update.sh
+++ update.sh
@@ -1,25 +1,26 @@
-Domain=""
-
 ZONE_ID=""
-RECORD_ID=""
-AUTH_EMAIL=""
-API_TOKEN=""
+DNS_RECORD_ID=""
+CLOUDFLARE_API_TOKEN=""
 
 Global_IP_Address=""
-
 IP_Address_Services=(
     "https://api.ipify.org/"
     "https://ipinfo.io/ip"
     "https://ipecho.net/plain"
 )
 patern="^([0-9]{1,3}\.){3}[0-9]{1,3}$"
+
 for service in ${IP_Address_Services[@]}
 do
-Global_IP_Address=$(curl -s "$service")
-if [[ $Global_IP_Address = $patern ]]; then
+Global_IP_Address=$(curl -s --connect-timeout 3 "$service")
+if [[ $Global_IP_Address = $pattern ]]; then
 break
 fi
 done
 
 echo $Global_IP_Address
 
+curl -X PATCH "https://api.cloudflare.com/client/v4/zones/$ZONE_ID/dns_records/$DNS_RECORD_ID" \
+    -H 'Content-Type: application/json' \
+    -H "Authorization: Bearer $CLOUDFLARE_API_TOKEN" \
+    -d "{\"content\": \"$Global_IP_Address\"}"
